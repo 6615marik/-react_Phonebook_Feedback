dca47b5e927146d7759342ec79da01ded00c78e2
Yen Maryna <113536885+6615marik@users.noreply.github.com>
2023-03-10 13:28:42 +0200

    c

---
+++ src/Form/Contacts.jsx
@@ -0,0 +1,8 @@
+export const Contacts = ({ contacts }) => {
+  return (
+    <>
+      <h2>Contacts</h2>
+      <ul></ul>
+    </>
+  );
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,15 @@ import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Noification } from './Notify/Notification';
+import { Form } from 'Form/Form';
+import { Contacts } from 'Form/Contacts';
 // import Notiflix from 'notiflix';
 export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
+    contacts: [],
   };
   onIncrement = e => {
     const { name } = e.target;
@@ -21,8 +24,12 @@ export class App extends Component {
   countPositiveFeedbackPercentage = () => {
     return Math.round((this.state.good / this.countTotalFeedback()) * 100);
   };
+
+  onFormData = data => {
+    console.log(data);
+  };
   render() {
-    const { good, bad, neutral } = this.state;
+    const { good, bad, neutral, contacts } = this.state;
     let total = this.countTotalFeedback();
     return (
       <>
@@ -43,6 +50,8 @@ export class App extends Component {
             />
           )}
         </Section>
+        <Form onSubmit={this.onFormData} />
+        <Contacts contacts={contacts} />
       </>
     );
   }
